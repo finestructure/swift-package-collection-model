5be9744a775b542a73122dd412c113660e4678de
Sven A. Schmidt <sas@finestructure.co>
2021-01-13 13:46:06 +0100

    Fix library name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "swift-package-collection-model",
+            name: "PackageCollectionModel",
             targets: ["swift-package-collection-model"]),
     ],
     dependencies: [
